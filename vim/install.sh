a6eecb080bd9beaf82a4e31715fc5056ef121579
William Bezuidenhout <william.bezuidenhout@gmail.com>
2015-02-20 11:13:52 +0200

    Renamed config files to be actual dotfiles for use with GNU stow

--- vim/install.sh
+++
@@ -1,4 +0,0 @@
-echo 'First installing vim-plug'
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-echo 'Linking vimrc to ~/.vimrc'
-ln -s $(pwd)/vimrc ~/.vimrc
