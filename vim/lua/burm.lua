ce57801965aab9db4c74b77fcdb901209b2fa2ec
William Bezuidenhout <william.bezuidenhout@gmail.com>
2022-02-09 23:12:35 +0200

    fix deprecated funcs

--- vim/lua/burm.lua
+++ vim/lua/burm.lua
@@ -180,7 +180,8 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
     buf_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>l', '<Cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<Cmd>lua vim.diagnostic.setqflist()<CR>', opts)
     buf_set_keymap('n', '<space>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
