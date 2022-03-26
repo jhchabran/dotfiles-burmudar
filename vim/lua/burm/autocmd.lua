7624914a80810ee39a15effc972dd7cde56c654c
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2022-03-26 17:58:38 +0200

    move autocmd

---
+++ vim/lua/burm/autocmd.lua
@@ -0,0 +1,19 @@
+--- Remove trailing whitespace before write
+vim.cmd([[
+    augroup BURMUDAR
+        autocmd!
+        autocmd BufWritePre * %s/\s\+$//e
+    augroup END
+    ]])
+
+vim.cmd([[
+    augroup PYTHON
+        autocmd BufNewFile,BufRead *.py set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab autoindent
+    augroup END
+    ]])
+
+vim.cmd([[
+    augroup RUST
+        autocmd BufNewFile,BufRead *.rs nnoremap <F5> :RustRun<CR>
+    augroup END
+    ]])
