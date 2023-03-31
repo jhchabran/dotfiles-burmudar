4790dd4243a2316b39631e6877c31cedb108a5dd
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2023-03-31 22:55:57 +0200

    rework some keybindings

--- vim/lua/burm/config.lua
+++ vim/lua/burm/config.lua
@@ -142,6 +142,11 @@ require('telescope').setup {
     file_previewer = require('telescope.previewers').vim_buffer_cat.new,
     grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
     qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key",
+      }
+    }
   },
   extensions = {
     fzf_native = {
