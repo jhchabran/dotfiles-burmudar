4977915660be3e5414d178d62e9f6c6c8e7b6287
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-04-14 23:09:26 +0200

    add alacritty to home manager

--- nix/home.nix
+++ nix/home.nix
@@ -263,6 +263,27 @@
     };
   };
 
+  programs.alacritty = {
+    enable = true;
+    package = pkgs.alacritty;
+    settings = {
+      window = {
+        decorations = "none";
+        padding = { x = 10; y = 10; };
+        startup_mode = "Maximized";
+        dynamic_title = true;
+        option_as_alt = "OnlyLeft";
+      };
+      font = {
+        size = 12.0;
+        normal = {
+          family = "JetBrainsMono Nerd Font Mono";
+          style = "Medium";
+        };
+      };
+    };
+  };
+
   # services
   services.gpg-agent = {
     enable = pkgs.stdenv.isLinux;
