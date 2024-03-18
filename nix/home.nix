3ed77aeab90a1ae3b6c3fa1362e96a3af5cd0532
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2024-03-18 22:07:13 +0200

    add atuin

--- nix/home.nix
+++ nix/home.nix
@@ -125,11 +125,19 @@ rec {
     # '';
   };
 
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
+
   programs.zsh = {
     enable = true;
-    historySubstringSearch = {
-      enable = true;
-    };
     syntaxHighlighting = {
       enable = true;
     };
